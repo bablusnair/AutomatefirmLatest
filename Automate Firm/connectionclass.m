acbf9632fbbf012e173793e2e2e81bfca57e5254
leonine <leonine@leonines-Mac-mini-10.local>
2017-01-13 15:07:43 +0530

    committed by hilaj

--- Automate Firm/connectionclass.m
+++ Automate Firm/connectionclass.m
@@ -16,6 +16,8 @@
 
 //online-server
 //tgtyyhyy6huuu7hyy7yghyhuyu
+//c scsnsscnkcncnc lcnkckl lcnksxlcds cdsklcjdlcdckl cdscdcmdsl dslcmdck
+
 
 NSString *const subDomainMainURL = @"http://automatefirm.com/automate_new/";
 NSString *const subdomainURL = @"http://192.168.1.35/af1.1/";
